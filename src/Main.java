f97b10ac2fc43a5943ff711bea91564c88d918b5
marekvu <marekvu1@email.cz>
2023-03-13 21:57:16 +0100

    applying changes

--- src/Main.java
+++ src/Main.java
@@ -3,189 +3,143 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Vyberte akci:");
-        System.out.println("1. nacteni souboru");
-        System.out.println("2. nacteni ze vstupu");
+    public static void main(String[] args) throws IOException {
+
+        String inputFile = args[0];
+        boolean readFromConsole = false;
 
 
-        int volba = Integer.parseInt(sc.nextLine());
-        switch (volba) {
-            case 1:
-                System.out.println("which file do you want to read ?");
-                System.out.println("1 - odd");
-                System.out.println("2 - even");
-                int choice = sc.nextInt();
-                if (choice == 1) {
+        if (args.length >= 2) {
+
+            File inputArgumentFile = new File(args[0]);
+            File outputArgumentFile = new File(args[1]);
 
 
-                    File file1 = new File("data\\licha.txt");
-                    File file2 = new File("data\\suda.txt");
-                    File file3 = new File("data\\output.txt");
+            try {
 
-                    if (!file1.exists() || !file2.exists() || !file3.exists()) {
-                        System.out.println("nelze nacist soubory");
-                        System.exit(0);
-                    }
+                BufferedReader br1 = new BufferedReader(new FileReader(inputArgumentFile));
+                ArrayList<Integer> numbers = new ArrayList<Integer>();
+                BufferedWriter bw = new BufferedWriter(new FileWriter(outputArgumentFile));
 
-                    try {
-                        BufferedReader br1 = new BufferedReader(new FileReader(file1));
-                        ArrayList<Integer> numbers = new ArrayList<Integer>();
-                        BufferedWriter bw = new BufferedWriter(new FileWriter(file3));
-
-                        String[] numbersString;
-                        String line1 = br1.readLine();
-                        numbersString = line1.split(",");
-                        int numberOfValues = numbersString.length;
-                        for (int i = 0; i < numberOfValues; i++) {
-                            numbers.add(Integer.parseInt(numbersString[i]));
-                        }
-
-                        if (numbers.size() % 2 == 0) {
-                            file3.getParentFile().mkdirs();
-                            for (int i = 0; i < numbers.size(); i++) {
-                                if (numbers.get(i) % 2 == 0) {
-                                    System.out.print(numbers.get(i) + " ");
-                                    bw.newLine();
-                                    bw.write(numbers.get(i) + " ");
-                                    bw.flush();
-                                }
-                            }
-
-
-                        } else if (numbers.size() % 2 != 0) {
-                            for (int i = 0; i < numbers.size(); i++) {
-                                if (numbers.get(i) % 2 != 0) {
-                                    System.out.print(numbers.get(i) + " ");
-                                }
-                            }
-
-                        }
-
-
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                    break;
-                }
-                if (choice == 2) {
-                    try {
-                        File file2 = new File("data\\suda.txt");
-                        BufferedReader br2 = new BufferedReader(new FileReader(file2));
-                        ArrayList<Integer> numbers = new ArrayList<Integer>();
-
-                        String[] numbersString;
-                        String line1 = br2.readLine();
-                        numbersString = line1.split(",");
-                        int numberOfValues = numbersString.length;
-                        for (int i = 0; i < numberOfValues; i++) {
-                            numbers.add(Integer.parseInt(numbersString[i]));
-                        }
-
-                        if (numbers.size() % 2 == 0) {
-                            for (int i = 0; i < numbers.size(); i++) {
-                                if (numbers.get(i) % 2 == 0) {
-                                    System.out.print(numbers.get(i));
-                                }
-                            }
-
-
-                        } else if (numbers.size() % 2 != 0) {
-                            for (int i = 0; i < numbers.size(); i++) {
-                                if (numbers.get(i) % 2 != 0) {
-                                    System.out.print(numbers.get(i));
-                                }
-                            }
-
-                        }
-
-
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                    break;
-
-
-                }
-            case 2:
-                try {
-                    File file3 = new File("data\\output.txt");
-                    BufferedWriter bw2 = new BufferedWriter(new FileWriter(file3));
-                    System.out.println("zadejte cisla oddelene carkou");
-                    String vstup = sc.nextLine();
-                    String[] znamky = vstup.split(",");
-
-
-                    if (znamky.length % 2 == 0) {
-                        file3.getParentFile().mkdirs();
-                        for (int i = 0; i < znamky.length; i++) {
-                            if (Integer.parseInt(znamky[i]) % 2 == 0) {
-                                System.out.println(znamky[i]);
-                                bw2.write(znamky[i] + " ");
-                            }
-                        }
-                        bw2.flush();
-
-                    } else if (znamky.length % 2 != 0) {
-                        for (int i = 0; i < znamky.length; i++) {
-                            if (Integer.parseInt(znamky[i]) % 2 != 0) {
-                                System.out.println(znamky[i]);
-                                bw2.write(znamky[i] + " ");
-                            }
-                        }
-                    }
-                    break;
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
+                String[] numbersString;
+                String line1 = br1.readLine();
+                numbersString = line1.split(",");
+                int numberOfValues = numbersString.length;
+                for (int i = 0; i < numberOfValues; i++) {
+                    numbers.add(Integer.parseInt(numbersString[i]));
                 }
 
+                if (numbers.size() % 2 == 0) {
+                    outputArgumentFile.getParentFile().mkdirs();
+                    for (int i = 0; i < numbers.size(); i++) {
+                        if (numbers.get(i) % 2 == 0) {
+                            System.out.print(numbers.get(i) + ",");
+                            bw.write(numbers.get(i) + ",");
+                            bw.flush();
+                        }
+                    }
+                    bw.close();
 
+                } else if (numbers.size() % 2 != 0) {
+                    for (int i = 0; i < numbers.size(); i++) {
+                        if (numbers.get(i) % 2 != 0) {
+                            System.out.print(numbers.get(i) + ",");
+                            bw.write(numbers.get(i) + ",");
+                            bw.flush();
+                        }
+                    }
+                    bw.close();
+
+                }
+
+            } catch (IOException f) {
+                throw new RuntimeException(f);
+            }
+        } else if (args.length == 1) {
+            try {
+                int num = Integer.parseInt(args[0]);
+                System.out.println("Reading input from console...");
+                readFromConsole = true;
+
+                Scanner sc = new Scanner(System.in);
+                System.out.println("zadejte cisla oddelene carkou");
+                String consoleInput1 = sc.nextLine();
+                System.out.println("Kam chcete čísla vypsat \n 1: do konzole \n 2: do souboru");
+                String consoleInput2 = sc.nextLine();
+                String[] consoleNumbers = consoleInput1.split(",");
+
+                if (consoleInput2.equals("1")) {
+                    if (consoleNumbers.length % 2 == 0) {
+                        for (int i = 0; i < consoleNumbers.length; i++) {
+                            if (Integer.parseInt(consoleNumbers[i]) % 2 == 0) {
+                                System.out.print(consoleNumbers[i] + ",");
+                            }
+
+                        }
+                    } else {
+                        for (int i = 0; i < consoleNumbers.length; i++) {
+                            if (Integer.parseInt(consoleNumbers[i]) % 2 == 0) {
+                                System.out.print(consoleNumbers[i] + ",");
+                            }
+                        }
+                    }
+                } else if (consoleInput2.equals("2")) {
+                    System.out.println("Zadejte cestu do vystupniho souboru");
+                    String pathName = sc.nextLine();
+                    File outputConsoleFile = new File(pathName);
+                    BufferedWriter bw1 = new BufferedWriter(new FileWriter(outputConsoleFile));
+                    outputConsoleFile.getParentFile().mkdirs();
+
+                    if (consoleNumbers.length % 2 == 0) {
+                        for (int i = 0; i < consoleNumbers.length; i++) {
+                            if (Integer.parseInt(consoleNumbers[i]) % 2 == 0) {
+                                System.out.println(consoleNumbers[i]);
+                                bw1.write(consoleNumbers[i] + " ");
+                            }
+                        }
+                        bw1.flush();
+
+                    }
+                } else {
+                    for (int i = 0; i < consoleNumbers.length; i++) {
+                        if (Integer.parseInt(consoleNumbers[i]) % 2 == 0) {
+                            System.out.println(consoleNumbers[i]);
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (NumberFormatException e) {
+                File inputfile1 = new File(inputFile);
+                BufferedReader br2 = new BufferedReader(new FileReader(inputfile1));
+                ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+                String[] numbersString2;
+                String line1 = br2.readLine();
+                numbersString2 = line1.split(",");
+                int numberOfValues = numbersString2.length;
+                for (int i = 0; i < numberOfValues; i++) {
+                    numbers.add(Integer.parseInt(numbersString2[i]));
+                }
+
+                if (numbers.size() % 2 == 0) {
+
+                    for (int i = 0; i < numbers.size(); i++) {
+                        if (numbers.get(i) % 2 == 0) {
+                            System.out.print(numbers.get(i) + " ");
+
+                        }
+                    }
+
+                } else if (numbers.size() % 2 != 0) {
+                    for (int i = 0; i < numbers.size(); i++) {
+                        if (numbers.get(i) % 2 != 0) {
+                            System.out.print(numbers.get(i) + " ");
+                        }
+                    }
+                }
+            }
         }
-
-
-//     File file1 = new File("data\\licha.txt");
-//     File file2 = new File("data\\suda.txt");
-//
-//     if (!file1.exists()||!file2.exists()){
-//         System.out.println("nelze nacist soubory");
-//       System.exit(0);
-//     }
-//
-//     try {
-//         BufferedReader br1 = new BufferedReader(new FileReader(file1));
-//         BufferedReader br2 = new BufferedReader(new FileReader(file2));
-//         ArrayList<Integer> numbers = new ArrayList<Integer>();
-//
-//
-//          String[] numbersString;
-//          String line1 = br1.readLine();
-//          numbersString = line1.split(",");
-//          int numberOfValues = numbersString.length;
-//          for (int i = 0;i<numberOfValues;i++){
-//              numbers.add(Integer.parseInt(numbersString[i]));
-//          }
-//
-//          for (int number : numbers){
-//               System.out.print(number + " ");
-//          }
-//          System.out.println();
-//          if (numbers.size()%2 ==0){
-//              System.out.println("sudy pocet");
-//          }
-//          else {
-//              System.out.println("lichy pocet");
-//          }
-//
-//
-//
-//
-//
-//
-//
-//     }catch (IOException e){
-//         throw new RuntimeException(e);
-//     }
-
     }
 }
+
